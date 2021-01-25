87f6728a2cf1b3735d8c8696a3cbc56a34b52eaa
jvictor23 <joaovictor00788@hotmail.com>
2021-01-25 13:20:31 -0300

    Criando rota de logout

--- app/routes/registroAutenticacao.route.js
+++ app/routes/registroAutenticacao.route.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const registroAutenticacao = require('../controllers/registroAutenticaco.controller')
 
 //controllers
-//rota de registro
+//rota de registro e autenticacao
 router.post("/registro", registroAutenticacao.registro);
 router.post("/login", registroAutenticacao.login);
+router.post("/logout", registroAutenticacao.logout);
 
 
 module.exports = router
