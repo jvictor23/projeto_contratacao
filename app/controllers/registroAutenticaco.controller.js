7738a7b5351e1e4c059f9957954e51f73cfe74a1
jvictor23 <joaovictor00788@hotmail.com>
2021-01-25 11:17:04 -0300

    Criando controller login

--- app/controllers/registroAutenticaco.controller.js
+++ app/controllers/registroAutenticaco.controller.js
@@ -21,5 +21,26 @@ module.exports={
                 data: null
             })
         }
+    },
+
+    login: async(req,res)=>{
+        //Destruturando dados
+        const{email,password} = req.body;
+        try {
+            //enviando dados para o model e recebendo seu retorno
+            const usuario = await model.login(email,senha);
+            //retornando token caso nao haja erro
+            return res.sen({
+                success: true,
+                data:null,
+                token: usuario.token
+            })
+        } catch (error) {
+            //retornando null caso haja erro
+            return res.send({
+                success: false,
+                data:null
+            })
+        }
     }
 }
