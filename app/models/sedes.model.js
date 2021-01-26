67ab8a62932cbff1dfed95b0b9e212783135c991
jvictor23 <joaovictor00788@hotmail.com>
2021-01-25 22:03:56 -0300

    Criando model putSede

--- app/models/sedes.model.js
+++ app/models/sedes.model.js
@@ -38,5 +38,39 @@ module.exports={
         const data = {id,cnpj,endereco,empresa_id,usuario_id};
 
         return data;
+    },
+
+    putSedes: async(id,cnpj, endereco, empresa_id, usuario_id)=>{
+
+        //verificando se id não é null, undefined ou ''
+        if(id === null || id === undefined || id === ''){
+            throw new Error("id está vazio");
+        }
+
+        //verificando se cnpj não é null, undefined ou ''
+        if(cnpj === null || cnpj === undefined || cnpj === ''){
+            throw new Error("CNJP está vazio");
+        }
+
+        //verificando se cnpj é invalido
+        if(!validator.cnpj.isValid(cnpj)){
+            throw new Error("CNPJ é inválido")
+        }
+
+        //verificando se endereco não é null, undefined ou ''
+        if(endereco === null || endereco === undefined || endereco === ''){
+            throw new Error("endereco está vazio");
+        }
+
+        //verificando se endereco não é null, undefined ou ''
+        if(empresa_id === null || empresa_id === undefined || empresa_id === ''){
+            throw new Error("empresa_id está vazio");
+        }
+
+        await repository.putSedes(id,cnpj,endereco,empresa_id,usuario_id);
+
+        const data ={id,cnpj,endereco,empresa_id,usuario_id};
+
+        return data;
     }
 }
