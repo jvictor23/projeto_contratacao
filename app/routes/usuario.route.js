3d641c79c7e4af6da436ebffc0530cdb40ece32b
jvictor23 <joaovictor00788@hotmail.com>
2021-01-26 10:52:12 -0300

    Criando rota putUsuarios

--- app/routes/usuario.route.js
+++ app/routes/usuario.route.js
@@ -5,6 +5,7 @@ const controller = require("../controllers/usuario.controller");
 //rotas
 router.get("/usuarios/:empresa_id", controller.getUsuarios);
 router.post("/usuarios/:empresa_id",controller.postUsuarios);
+router.put("/usuarios/:empresa_id/:usuario_id", controller.putUsuarios);
 
 
 module.exports = router;
