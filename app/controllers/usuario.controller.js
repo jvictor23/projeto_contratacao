2b76b8cc58dcfc2ecc39bb772823eacf320fd0d4
jvictor23 <joaovictor00788@hotmail.com>
2021-01-26 10:36:46 -0300

    Criando controller postUsuarios

--- app/controllers/usuario.controller.js
+++ app/controllers/usuario.controller.js
@@ -22,5 +22,30 @@ module.exports={
                 data: null
             })
         }
+    },
+
+    postUsuarios: async(req,res)=>{
+        // Destruturando dados
+        const {nome,email,password,usuarios_id} = req.body;
+        const {empresa_id} = req.params;
+
+        try {
+
+            //enviando e recebendo dados do model
+            const usuario = await model.postUsuarios(nome,email,password,empresa_id,usuarios_id);
+
+            //retornado {} de usuario
+            return res.send({
+                success:true,
+                data: usuario
+            })
+            
+        } catch (error) {
+             //retornando false e null caso haja erro
+             return res.send({
+                success: false,
+                data: null
+            })
+        }
     }
 }
