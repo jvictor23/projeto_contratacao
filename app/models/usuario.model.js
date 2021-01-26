dc54d24e8f3176b416f73d6cf79547d539718417
jvictor23 <joaovictor00788@hotmail.com>
2021-01-26 10:31:24 -0300

    Criando model postUsuarios

--- app/models/usuario.model.js
+++ app/models/usuario.model.js
@@ -10,5 +10,44 @@ module.exports={
         const usuario = await repository.getUsuarios(empresa_id);
 
         return usuario;
+    },
+
+    postUsuarios: async(nome,email,password,empresa_id,usuario_id)=>{
+        //regex para validacao de email
+        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+        //verificando se nome não é null, undefined ou ''
+        if(nome === null || nome === undefined || nome === ''){
+            throw new Error("nome está vazio");
+        }
+
+          //verificando se email não é null, undefined ou ''
+          if(email === null || email === undefined || email === ''){
+            throw new Error("email está vazio");
+        }
+
+         //verificando se email é valido com o regex
+         if(!reg.test(email)){
+            console.log("O email é inválido")
+            throw new Error("O email é inválido")
+        }
+
+        //verificando se password não é null, undefined ou ''
+        if(password === null || password === undefined || password === ''){
+            throw new Error("password está vazio");
+        }
+
+        //verificando se empresa_id não é null, undefined ou ''
+        if(empresa_id === null || empresa_id === undefined || empresa_id === ''){
+            throw new Error("empresa_id está vazio");
+        }
+
+        //enviando e recebendo dados do repository
+        const id = repository.postUsuarios(nome,email,password,empresa_id,usuario_id);
+
+        //adicionando id aos dados ja existente
+        const data={id,nome,email,password,empresa_id,usuario_id};
+
+        return data;
     }
 }
