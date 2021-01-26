f3517c7f69739b324d82644f5c178d2e93c084d7
jvictor23 <joaovictor00788@hotmail.com>
2021-01-25 21:52:11 -0300

    Criando controller postSedes

--- app/controllers/sedes.controller.js
+++ app/controllers/sedes.controller.js
@@ -16,5 +16,24 @@ module.exports={
                 data: null
             })
         }
+    },
+
+    postSedes: async(req,res)=>{
+        const {cnpj, endereco, usuario_id} = req.body;
+        const {empresa_id} = req.params;
+
+        try {
+            const sede = await model.postSedes(cnpj,endereco,empresa_id,usuario_id);
+
+            return res.send({
+                success: true,
+                data: sede
+            })
+        } catch (error) {
+            return res.send({
+                success: false,
+                data: null
+            })
+        }
     }
 }
