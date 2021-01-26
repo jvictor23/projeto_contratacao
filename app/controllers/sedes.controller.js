03e6e09754e10c11bd053af5289bedad3db727b2
jvictor23 <joaovictor00788@hotmail.com>
2021-01-25 22:06:21 -0300

    Criando controller putSedes

--- app/controllers/sedes.controller.js
+++ app/controllers/sedes.controller.js
@@ -35,5 +35,24 @@ module.exports={
                 data: null
             })
         }
+    },
+
+    putSedes: async(req,res)=>{
+        const {cnpj, endereco, usuario_id} = req.body;
+        const {empresa_id, sede_id} = req.params;
+
+        try {
+            const sede = await model.putSedes(sede_id,cnpj,endereco,empresa_id,usuario_id);
+
+            return res.send({
+                success: true,
+                data: sede
+            })
+        } catch (error) {
+            return res.send({
+                success: false,
+                data: null
+            })
+        }
     }
 }
